e3bc64e7d790035d6d6b90dc0c2b9160790a273d
Caroline Bridge <cbridge@oicr.on.ca>
2022-10-14 12:28:30 -0400

    add waits to page loading

--- cypress/integration/3_nav_filter_widget.ts
+++ cypress/integration/3_nav_filter_widget.ts
@@ -2,6 +2,7 @@ describe('nav filter widget', () => {
   it('visits JBrowse ICGC session', () => {
     cy.exec('shx cp cypress/fixtures/icgc_session.json .jbrowse')
     cy.visit('/?config=icgc_session.json')
+    cy.wait(1000)
 
     // The plugin successfully loads
     cy.contains('Open').click()
